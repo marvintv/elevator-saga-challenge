ee3cdca870dca780db1a72b8293842e3945c1819
Marvin Vilaysack <marvin@Marvins-MBP.hsd1.mn.comcast.net>
2021-05-11 22:27:34 -0500

    updated challenge 4

--- elevator.js
+++ elevator.js
@@ -7,26 +7,64 @@
    Challenge #3 
    No Changes
 
+   Challenge #4
+   Handled two elevators and put it inside a for loop. 
+   Added the listener event for floor_button_pressed and went to that floorNum with priority, and transversed through floors 0-7.
+
 */
-
 {
-    
-    init: function(elevators, floors) {
-        var elevator = elevators[0]; // Let's use the first elevator
 
-        // Whenever the elevator is idle (has no more queued destinations) ...
-        elevator.on("idle", function() {
-            // let's go to all the floors (or did we forget one?)
-            elevator.goToFloor(0);
-            elevator.goToFloor(1);
-            elevator.goToFloor(2); 
-            elevator.goToFloor(3); 
-            elevator.goToFloor(4);
-        });
+    init: function(elevators, floors) {
+
+        for (var i = 0;  i < 2;  i++){
+            let elevator = elevators[i];
+            elevator.on("idle", function() {
+                elevator.goToFloor(0);
+                elevator.goToFloor(1);
+                elevator.goToFloor(2); 
+                elevator.goToFloor(3); 
+                elevator.goToFloor(4);
+                elevator.goToFloor(5);
+                elevator.goToFloor(6);
+                elevator.goToFloor(7);
+            });
+
+            elevator.on("floor_button_pressed", function(floorNum) {elevator.goToFloor(floorNum, true)} );
+
+        }
+        // var elevator = elevators[0]; // Let's use the first elevator
+        // var elevator2 = elevators[1]; // second elevator
+
+        // // Whenever the elevator is idle (has no more queued destinations) ...
+        // elevator.on("idle", function() {
+        //     // let's go to all the floors (or did we forget one?)
+        //     elevator.goToFloor(0);
+        //     elevator2.goToFloor(1);
+        //     elevator2.goToFloor(2); 
+        //     elevator2.goToFloor(3); 
+        //     elevator2.goToFloor(4);
+        //     elevator2.goToFloor(5);
+        //     elevator2.goToFloor(6);
+        //     elevator.goToFloor(7);
+        // });
+
+        // elevator2.on("idle", function() {
+        //     // let's go to all the floors (or did we forget one?)
+        //     elevator2.goToFloor(0);
+        //     elevator2.goToFloor(1);
+        //     elevator2.goToFloor(2); 
+        //     elevator2.goToFloor(3); 
+        //     elevator2.goToFloor(4);
+        //     elevator2.goToFloor(5);
+        //     elevator2.goToFloor(6);
+        //     elevator2.goToFloor(7);
+        // });
+
+
 
 
     },
-    update: function(dt, elevators, floors) {
-        // We normally don't need to do anything here
-    }
+        update: function(dt, elevators, floors) {
+            // We normally don't need to do anything here
+        }
 }
