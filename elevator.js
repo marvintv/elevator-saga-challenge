9f768bdfb8c9dfa3c879c003db68f018fa650444
Marvin Vilaysack <marvin@Marvins-MBP.hsd1.mn.comcast.net>
2021-05-11 23:07:49 -0500

    updated Challenge 5

--- elevator.js
+++ elevator.js
@@ -1,52 +1,39 @@
 /* Challenge #1
    Added a single floor to handle the elevator on idle.
-
    Challenge #2 
    Added another two floors to the code to account for floors 0 to 4
-
    Challenge #3 
    No Changes
-
    Challenge #4
    Handled two elevator objects, which traversed from floors 0 to 7
    Added the  floor_button_pressed listener event for both elevators, which went to floorNum with priority.
-
+   Challenge #5
+   intitilized numOfElevators and numOfFloors to a var. We traversed through each elevator using a for loop. Added another for loop for going through floors because num was changing.
 */
+
 {
 
     init: function(elevators, floors) {
-        var elevator = elevators[0]; // Let's use the first elevator
-        var elevator2 = elevators[1]; // second elevator
 
-        // Whenever the elevator is idle (has no more queued destinations) ...
-        elevator.on("idle", function() {
-            // let's go to all the floors (or did we forget one?)
-            elevator.goToFloor(0);
-            elevator.goToFloor(1);
-            elevator.goToFloor(2); 
-            elevator.goToFloor(3); 
-            elevator.goToFloor(4);
-            elevator.goToFloor(5);
-            elevator.goToFloor(6);
-            elevator.goToFloor(7);
-        });
+        var numOfElevators = elevators.length;
+        var numOfFloors = floors.length; 
+        for (var i = 0;  i < numOfElevators;  i++){
+            let elevator = elevators[i];
+            elevator.on("idle", function() {
+                console.log("Elevator: " + i);
 
-        elevator2.on("idle", function() {
-            // let's go to all the floors (or did we forget one?)
-            elevator2.goToFloor(0);
-            elevator2.goToFloor(1);
-            elevator2.goToFloor(2); 
-            elevator2.goToFloor(3); 
-            elevator2.goToFloor(4);
-            elevator2.goToFloor(5);
-            elevator2.goToFloor(6);
-            elevator2.goToFloor(7);
-        });
+                for (var j = 0; j < numOfFloors; j++){ //second for loop for floors
+                    elevator.goToFloor(j);
+                }
 
+            });
 
-        elevator.on("floor_button_pressed", function(floorNum) {elevator.goToFloor(floorNum, true)} );
-        elevator2.on("floor_button_pressed", function(floorNum) {elevator2.goToFloor(floorNum, true)} );
+            elevator.on("floor_button_pressed", function(floorNum) {
+                console.log("floor pressed. Floor num: " + floorNum);
+                elevator.goToFloor(floorNum, true)
+            } );
 
+        }
 
     },
         update: function(dt, elevators, floors) {
